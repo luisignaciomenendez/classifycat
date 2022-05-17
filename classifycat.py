d2d4b2d9ab25e99e08618687d3063670a718abcc
Luis I. Menéndez <62380208+luisignaciomenendez@users.noreply.github.com>
2022-05-17 14:53:59 +0200

    Update classifycat.py

--- classifycat.py
+++ classifycat.py
@@ -190,9 +190,9 @@ def _text(tweet, i, width=75):
     body.append(
         _border(
             click.style(
-                '@' + tweet['author.username'][i]
-                + ' - '
-                + tweet['author.name'][i],
+                '@' +
+                 ' - '
+                ,
                 fg='yellow'
             ),
             width
@@ -225,10 +225,10 @@ def _text(tweet, i, width=75):
         created = ''
     #m = tweet['public_metrics'][i]
     metrics = (
-        f'♡ {tweet["public_metrics.like_count"][i]}  '
-        f'♺ {tweet["public_metrics.retweet_count"][i]}  '
-        f'↶ {tweet["public_metrics.reply_count"][i]}  '
-        f'« {tweet["public_metrics.quote_count"][i]}'
+        f'♡ {}  '
+        f'♺ {}  '
+        f'↶ {}  '
+        f'« {}'
     )
 
     padding = (width - 4 - wcswidth(created + metrics)) * ' '
