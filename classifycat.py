1620f932820ee0d5c34c3e56e48cc9c317a9cd9b
Luis I. Menéndez <62380208+luisignaciomenendez@users.noreply.github.com>
2022-05-19 16:15:01 +0200

    Update classifycat.py

--- classifycat.py
+++ classifycat.py
@@ -59,7 +59,7 @@ def cli(infile, new,os):
         # last = existing['index'].iloc[-1]
         click.secho(f'Starting classification where you left it, in row {last}',
                     fg='yellow', bg='red', bold=True)
-        df = existing.loc[last:]
+        df = existing.loc[last+1:]
         infile = click.prompt(
             'Name to store the new file [name without .csv]')
 
